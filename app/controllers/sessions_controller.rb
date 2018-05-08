780b231c95b3d12e3ff4d9368401ab1b3fb4b4f4
Joseph Papaluca <jpapaluc@gmail.com>
2018-05-08 15:21:00 -0400

    log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,15 @@ class SessionsController < ApplicationController
   def create
     admin = Admin.find_by(email: params[:session][:email].downcase)
     if admin && admin.authenticate(params[:session][:password])
-      log_in admin 
+      log_in admin
     else
       render 'new'
     end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 
 end
